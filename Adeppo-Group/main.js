af78fda46d376b5a47987623dbd36d3b2d692e13
Devjosh <jocalex8@gmail.com>
2023-06-05 22:42:47 +0100

    done with the page still need to do some javascrip

--- Adeppo-Group/main.js
+++ Adeppo-Group/main.js
@@ -177,6 +177,16 @@ window.addEventListener('DOMContentLoaded', function () {
 });
 
 
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
 
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
 
 console.log("hey there");
